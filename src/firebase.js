714c1273f70fcca3c7a4297f9f44c2c03991b30b
parresh77 <parresh77@gmail.com>
2021-06-22 23:56:23 +0530

    :tada: Start React Project with Redux Template
    Project created with 'create-react-app'.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArGcLeljuBP4yewvOU_VsXPIzJd9Pjhd4",
+  authDomain: "netflix-clone7737.firebaseapp.com",
+  projectId: "netflix-clone7737",
+  storageBucket: "netflix-clone7737.appspot.com",
+  messagingSenderId: "303790074995",
+  appId: "1:303790074995:web:30d569c90109e8cb8c8e06"
+};
